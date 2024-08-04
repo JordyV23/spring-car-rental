ed1a65220900b909fa202565fc1a62e39c36b1a1
JordyV23 <jordyvargas0123@gmail.com>
2024-08-03 22:46:55 -0600

    Commit inicial

---
+++ backend/CarRentalSpring/src/test/java/com/dev23/CarRentalSpring/CarRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev23.CarRentalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
